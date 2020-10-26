453ab2a9fb5ccadb36a2007716c66c50a626a2f5
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-10-26 08:45:47 +0530

    get the current time with Date object

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
 
+//get the current time
+var date = new Date();
+var hr = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+
+console.log(hr + " Hours " + min + " minutes " + sec + " seconds");
+
 //injecting in-line CSS styles
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
