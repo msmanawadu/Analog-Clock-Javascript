3ec72667854db4d2e48ed5c55b1038fbf6b63113
Sandeepa Manawadu <msmanawadu@gmail.com>
2020-10-26 07:45:26 +0530

    Apply CSS transform property as an in-line CSS style, using JS

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+//Arm turning degrees
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+//injecting in-line CSS styles
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+
+
